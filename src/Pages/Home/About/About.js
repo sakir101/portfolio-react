f8644c5f5cf8c1b2cad62a1e5de3d33bd589483e
sakir101 <sakirhossainf99@gmail.com>
2022-12-11 21:25:56 +0600

    resume download

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -11,7 +11,7 @@ const About = () => {
                 <div className="hero-content flex-col lg:flex-row">
                     <img src={sakir} alt='sakir' className=" w-full lg:w-1/3 rounded-lg shadow-2xl" />
                     <div className='w-full lg:w-1/2 mx-0 lg:mx-5'>
-                        <h1 className="text-2xl font-bold text-center lg:text-left">Person love to create</h1>
+                        <h1 className="text-2xl font-bold text-center lg:text-left text-slate-400">Person love to create</h1>
                         <h1 className="text-3xl font-bold text-center lg:text-left">Modern Technology</h1>
                         <p className="pt-6 pb-3 text-center lg:text-left">I like to call myself “a professional computer science student & Web developer”.</p>
                         <p className="pb-6 text-center lg:text-left">I made lot of creative activites. My first priority is to understand anything from heart. So that I can use that thing in future practically.</p>
